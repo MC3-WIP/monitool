0eca0cbdb724cfb88de55ffbdcf86979d92ae3be
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-07-21 15:40:38 +0700

    Initial Commit

---
+++ monitool/monitoolApp.swift
@@ -0,0 +1,20 @@
+//
+//  monitoolApp.swift
+//  monitool
+//
+//  Created by Christianto Budisaputra on 21/07/21.
+//
+
+import SwiftUI
+
+@main
+struct monitoolApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
