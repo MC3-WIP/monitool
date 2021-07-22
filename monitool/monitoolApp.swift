e6b88e9499f7ea4662f6fda53108d073e88aef5f
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-07-23 04:27:57 +0700

    Setup CoreData and CloudKit integration

--- monitool/monitoolApp.swift
+++ monitool/monitoolApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @main
 struct monitoolApp: App {
-    let persistenceController = PersistenceController.shared
+	let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
