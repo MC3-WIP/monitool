ee04ee81144e7c0781e27d78bf04b9e68fc740e0
Albertus Favian <favianalbert@gmail.com>
2021-07-23 11:36:42 +0700

    first localization, waiting for final hifi to recheck

--- monitool/Locales/Strings.swift
+++ monitool/Locales/Strings.swift
@@ -13,10 +13,58 @@ import Foundation
 enum Strings {
 	// MARK: Page
 	/// Task List Page
-	enum TaskList {
-		static let title = "tasklist.title".localized
+    enum Onboarding {
+        static let signIn = "onboarding.signIn".localized
+    }
+    
+    enum CompanyProfileOnboarding {
+        static let title = "CompanyProfileOnboarding.title".localized
+        static let placeholder = "CompanyProfileOnboarding.placeholder".localized
+        static let companyNameTitle = "CompanyProfileOnboarding.companyNameTitle".localized
+        static let saveBtn = "CompanyProfileOnboarding.saveBtn".localized
+        static let employeeTitle = "CompanyProfileOnboarding.employeeTitle".localized
+    }
+    
+    enum AddTask {
+        static let title = "AddTask.title".localized
+        static let descriptionTitle = "AddTask.descriptionTitle".localized
+        static let repeatTitle = "AddTask.repeatTitle".localized
+        static let titleTitle = "AddTask.titleTitle".localized
+        static let placeholder = "AddTask.placeholder".localized
+    }
+    
+	enum ToDoList {
+        static let title = "ToDoList.title".localized
+        static let statusTask = "ToDoList.statusTask".localized
 	}
+    
+    enum TaskDetail{
+        static let title = "TaskDetail.title".localized
+        static let approveBtn = "TaskDetail.approveBtn".localized
+        static let reviseBtn = "TaskDetail.reviseBtn".localized
+        static let takePhotoBtn = "TaskDetail.takePhotoBtn".localized
+    }
+    
+    enum PopOverRepeatAddTask {
+        static let repeatTitle = "PopOverRepeatAddTask.repeatTitle".localized
+        static let sunday = "PopOverRepeatAddTask.sunday".localized
+        static let monday = "PopOverRepeatAddTask.monday".localized
+        static let tuesday = "PopOverRepeatAddTask.tuesday".localized
+        static let wednesday = "PopOverRepeatAddTask.wednesday".localized
+        static let thursday = "PopOverRepeatAddTask.thursday".localized
+        static let friday = "PopOverRepeatAddTask.friday".localized
+        static let saturday = "PopOverRepeatAddTask.saturday".localized
+    }
+    
+    enum PinModal {
+        static let title = "PinModal.title".localized
+    }
+    
+    enum Profile {
+        static let switchToRoleBtn = "Profile.switchToRoleBtn".localized
+    }
 }
+
 
 extension String {
 	var localized: String {
