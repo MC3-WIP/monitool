21cb2f7e2762227a082e81d163562b6c71094c32
Albertus Favian <favianalbert@gmail.com>
2021-08-07 11:51:52 +0700

    add todaylist view

--- monitool/Views/Main/iPad Components/TaskListView.swift
+++ monitool/Views/Main/iPad Components/TaskListView.swift
@@ -25,7 +25,7 @@ struct TaskListView: View {
 		List {
 			ForEach(filteredData) { task in
 				NavigationLink(
-					destination: ReviseTaskDetailView()) {
+					destination: TaskDetailWaitingOwenerReviewView()) {
 					TaskListRow(task: task)
 				}
 			}
---
+++ monitool/Views/Main/iPad Components/TodayListView.swift
@@ -0,0 +1,95 @@
+//
+//  TodayListView.swift
+//  monitool
+//
+//  Created by Mac-albert on 07/08/21.
+//
+
+import SwiftUI
+
+struct TodayListView: View {
+    private let pic: String = "-"
+    private let notes: String = "-"
+    // MARK: INITIALIZE TOTAL PAGE
+    private let totalPage: Int = 3
+    
+    @State var proofPage = 0
+    
+    var body: some View {
+        NoSeparatorList{
+            HStack{
+                GeometryReader{ metric in
+                    VStack{
+                        Text("Buka Gerbang Toko!")
+                            .font(.system(size: 28, weight: .bold))
+                            .padding(.vertical, 24.0)
+                            .frame(minWidth: 100, maxWidth: .infinity, minHeight: 28, maxHeight: 32, alignment: .leading)
+                        Image("kucing1")
+                            .resizable()
+                            .frame(width: metric.size.width * 0.75, height: metric.size.width * 0.75, alignment: .leading)
+                        Text("Masukkan saja kawat ke lubang kunci dan gerak-gerakkan searah dengan jarum jam. Jika digerakkan berlawanan dengan arah jarum jam sama saja anda menguncinya dan berujung sia-sia. Walaupun demikian, jangan terlalu bergantung pada cara ini, karena bisa saja kualitas kunci yang baik tidak mudah jika dilakukan hal-hal yang tidak sesuai ketentuan dalam proses membuka pintu yang terkunci")
+                            .fixedSize(horizontal: false, vertical: true)
+                            .font(.system(size: 17))
+                            .multilineTextAlignment(.leading)
+                    }
+                    .padding(.leading, 18.0)
+                }
+                GeometryReader{ matric in
+                    VStack(spacing: 8){
+                        Text("Proof of Work")
+                            .padding(.bottom, 8)
+                            .font(.system(size: 20, weight: .bold))
+                            .frame(minWidth: 100, maxWidth: .infinity, minHeight: 20, maxHeight: 24, alignment: .leading)
+                            .foregroundColor(Color(hex: "898989"))
+                        VStack{
+                            ProofOfWork(image: "kucing2", date: "p", metricSize: matric)
+                        }
+                        .frame(width: matric.size.width * 0.75, height: matric.size.width * 0.75)
+                        .padding(.vertical, 10)
+                        .background(Color(hex: "F0F9F8"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color(hex: "4EB0AB"), lineWidth: 1)
+                        )
+                        HStack{
+                            Text("PIC: ")
+                                .foregroundColor(Color(hex: "6C6C6C"))
+                                .font(.system(size: 17, weight: .bold))
+                            Text(pic)
+                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 21, alignment: .leading)
+                        .padding(.top, 27)
+                        HStack{
+                            Text("Notes: ")
+                                .foregroundColor(Color(hex: "6C6C6C"))
+                                .font(.system(size: 17, weight: .bold))
+                            Text(notes)
+                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 21, alignment: .leading)
+                        .padding(.top, 20)
+                        
+                    }.frame(alignment: .leading)
+                }
+                
+                
+            }
+        }
+    }
+    @ViewBuilder
+    func ProofOfWork(image: String, date: String, metricSize: GeometryProxy) -> some View{
+        VStack{
+            Image(image)
+                .resizable()
+                .frame(width: metricSize.size.width * 0.5, height: metricSize.size.width * 0.5)
+//            Text("21 Jul 2021 at 15:57")
+//                .font(.system(size: 11))
+//                .frame(width: metricSize.size.width * 0.7, height: 12, alignment: .leading)
+        }
+    }
+}
+
+struct TodayListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodayListView()
+            .previewDevice("iPad Pro (12.9-inch) (5th generation)")
+            .previewLayout(.fixed(width: 1112, height: 834))
+    }
+}
