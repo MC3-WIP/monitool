57aa35d3f266c7e192cff32138bd84f473785a1a
devin1727 <devinwinardi1727@gmail.com>
2021-08-13 12:23:04 +0700

    merge conflict

--- monitool/Repositories/TaskRepository.swift
+++ monitool/Repositories/TaskRepository.swift
@@ -17,16 +17,36 @@ final class TaskRepository: ObservableObject {
     private let storage = Storage.storage()
     
     @Published var tasks: [Task] = []
+    @Published var histories: [Task] = []
+    @Published var completedTasks: [Task] = []
 
     init(){
 		let dummyTask = Task(name: "beli kopi")
 		add(dummyTask)
         get()
+        getHistory()
+    }
+    
+    func getHistory(){
+        store.collection(path.task).whereField("isHistory", isEqualTo: true)
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    print("Error getting stories: \(error.localizedDescription)")
+                    return
+                }
+                
+                let histories = querySnapshot?.documents.compactMap {document in
+                    try? document.data(as: Task.self)
+                } ?? []
+
+                DispatchQueue.main.async {
+                    self.histories = histories
+                }
+            }
     }
     
     func get(){
-//		store.collection(path.task)
-		store.collection("companies/mBz4gtAwSyQA8JDWhePN/tasks")
+        store.collection(path.task).whereField("isHistory", isEqualTo: false)
             .addSnapshotListener { querySnapshot, error in
                 if let error = error {
                     print("Error getting stories: \(error.localizedDescription)")
--- monitool/Views/Main/iPad Components/HistoryView.swift
+++ monitool/Views/Main/iPad Components/HistoryView.swift
@@ -8,22 +8,83 @@
 import SwiftUI
 
 struct HistoryView: View {
-    var tasks = TaskListViewModel().tasks
+    @ObservedObject var taskViewModel = TaskViewModel()
+    let dateHelper: DateHelper = DateHelper()
+    
+    init(){
+        UITableView.appearance().backgroundColor = .clear
+    }
     
     var body: some View {
         List{
-            Section(header: Text("Today").font(.title).foregroundColor(.black).fontWeight(.bold)){
-                HistoryRow()
-                HistoryRow()
-                HistoryRow()
+            ForEach(0..<6){ index in
+                if taskViewModel.historiesPerDay[index].count == 0{
+                    EmptyView()
+                }
+                else{
+                    HistoriesSection(histories: taskViewModel.historiesPerDay[index])
+                        .padding(.vertical, 5)
+                }
             }
-            .textCase(nil)
-            Section(header: Text("Yesterday").font(.title).foregroundColor(.black).fontWeight(.bold)){
-                HistoryRow()
-                HistoryRow()
-                HistoryRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+        .background(Color.white)
+    }
+//        List {
+//            Section(header: Text("Today").font(.title).foregroundColor(.black).fontWeight(.bold)){
+//                ForEach(taskViewModel.tasks, id: \.id){ task in
+//                    if(dateHelper.getNumDays(first: Date(), second: task.createdAt) == 0 && task.status.rawValue == "Completed"){
+//                        HistoryRow(task: task)
+//                    }
+//                }
+//            }
+//            .textCase(nil)
+//            .listRowBackground(AppColor.primaryForeground)
+//
+//            Section(header: Text("Yesterday").font(.title).foregroundColor(.black).fontWeight(.bold)){
+//                ForEach(taskViewModel.tasks, id: \.id){ task in
+//                    if(dateHelper.getNumDays(first: task.createdAt, second: Date()) == 1 && task.status.rawValue == "Completed"){
+//                        HistoryRow(task: task)
+//                    }
+//                }
+//            }
+//            .textCase(nil)
+//
+//            Section(header: Text(dateHelper.getStringFromDate(date: min2Day!)).font(.title).foregroundColor(.black).fontWeight(.bold)){
+//                ForEach(taskViewModel.tasks, id: \.id){ task in
+//                    if(dateHelper.getNumDays(first: task.createdAt, second: Date()) == 2 && task.status.rawValue == "Completed"){
+//                        HistoryRow(task: task)
+//                    }
+//                }
+//            }
+//            .textCase(nil)
+//        }
+//        .listStyle(PlainListStyle())
+//		.navigationTitle("History")
+//    }
+        
+}
+
+struct HistoriesSection: View{
+    var dateHelper = DateHelper()
+    var histories: [Task]
+    var day: String
+    
+    init(histories: [Task]){
+        self.histories = histories
+        if dateHelper.getNumDays(first: histories[0].createdAt, second: Date()) == 0 {
+            self.day = "Today"
+        }
+        else{
+            self.day = dateHelper.getStringFromDate(date: histories[0].createdAt)
+        }
+    }
+    
+    var body: some View{
+        Section(header: Text(day).font(.title).foregroundColor(.black).fontWeight(.bold)){
+            ForEach(histories, id: \.id){ history in
+                HistoryRow(task: history)
             }
-            .textCase(nil)
         }
         .listStyle(GroupedListStyle())
         .background(Color.white)
@@ -31,10 +92,12 @@ struct HistoryView: View {
 }
 
 struct HistoryRow: View {
+    let task: Task
+    
     var body: some View{
         VStack(alignment: .leading){
-            Text("Todo")
-            Text("PIC: Devin").font(.caption).foregroundColor(Color(hex: "#7A7A7A"))
+            Text(task.name)
+            Text("PIC: ").font(.caption).foregroundColor(Color(hex: "#7A7A7A"))
         }.listRowBackground(Color(hex: "#F0F9F8"))
     }
 }
@@ -42,5 +105,25 @@ struct HistoryRow: View {
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
         HistoryView()
+    }
+}
+
+struct DateHelper {
+    let dateFormatter = DateFormatter()
+    func getNumDays(first: Date, second: Date) -> Int {
+        let calendar = Calendar.current
+
+        // Replace the hour (time) of both dates with 00:00
+        let date1 = calendar.startOfDay(for: first)
+        let date2 = calendar.startOfDay(for: second)
+
+        let components = calendar.dateComponents([.day], from: date1, to: date2)
+        return components.day!
+    }
+    
+    func getStringFromDate(date: Date) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMMM YYYY"
+        return dateFormatter.string(from: date)
     }
 }
