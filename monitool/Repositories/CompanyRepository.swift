495bd49832f77b8ac537168f6e6966537d23e72a
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-08-18 18:21:19 +0700

    Remove unused code in CompanyRepository

--- monitool/Repositories/CompanyRepository.swift
+++ monitool/Repositories/CompanyRepository.swift
@@ -10,7 +10,6 @@ import Combine
 import FirebaseAuth
 
 final class CompanyRepository: ObservableObject {
-	@Published var companies = [Company]()
 	private let paths = RepositoriesPath()
 	private let store = Firestore.firestore()
 	var companyRef: DocumentReference?
@@ -30,31 +29,5 @@ final class CompanyRepository: ObservableObject {
 			"profileImage": "profile",
 			"ownerPin": company.ownerPin
 		])
-	}
-
-	func addImage(imageURL: String) {
-		companyRef?.updateData(["profileImage": imageURL])
-	}
-
-	func delete(_ company: Company) {
-		store.collection(paths.company).document(company.id).delete()
-	}
-
-	func editCompanyName(name: String) {
-		companyRef?.updateData(["name": name])
-	}
-
-	func editCompanyMinReview(minReview: Int) {
-		companyRef?.updateData(["minReview": minReview])
-	}
-
-	// MARK: EDIT PHOTO BELOM
-
-	func approveTask(status: String) {
-
-	}
-
-	func rejectTask(status: String) {
-
 	}
 }
