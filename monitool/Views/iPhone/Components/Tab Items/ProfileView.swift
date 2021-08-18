12a30447b76e29c84282a99120463e5d4aea2a6b
devin1727 <devinwinardi1727@gmail.com>
2021-08-18 20:18:23 +0700

    Get data history for iphone and segue from history to history detail

--- monitool/Views/iPhone/Components/Tab Items/ProfileView.swift
+++ monitool/Views/iPhone/Components/Tab Items/ProfileView.swift
@@ -8,27 +8,23 @@
 import SwiftUI
 
 struct ProfileView: View {
+
     @Environment(\.presentationMode) var presentationMode
 	@StateObject var companyViewModel = CompanyViewModel()
     @StateObject var profileViewModel = ProfileViewModel()
 
     @ObservedObject var employeeListViewModel = EmployeeListViewModel()
-    @ObservedObject var role: RoleService = .shared
 
     var company: Company?
 
     @State var companyName = ""
-    @State var companyPIN = ""
 	@State var editMode: EditMode = .inactive {
 		didSet {
 			if editMode.isEditing { profileViewModel.isPinHidden = false } else { profileViewModel.isPinHidden = true }
 		}
 	}
-    
-    init(){
-        self.companyName = profileViewModel.company.name
-        self.companyPIN = profileViewModel.company.ownerPin
-    }
+
+	@ObservedObject var role: RoleService = .shared
 
 	var body: some View {
 		VStack {
@@ -39,12 +35,12 @@ struct ProfileView: View {
 						CompanyInfoTextField(
 							title: "Company Name",
 							placeholder: "Company Inc.",
-                            text: $companyName
+                            text: $profileViewModel.company.name
 						)
 						CompanyInfoTextField(
 							title: "Owner PIN",
 							placeholder: "1234",
-							text: $companyPIN
+							text: $profileViewModel.company.ownerPin
 						)
 					}
 					ReviewPolicy()
@@ -102,7 +98,7 @@ extension ProfileView {
 			}
 			if !editMode.isEditing {
 				HStack {
-                    Text(companyName)
+                    Text(profileViewModel.company.name)
                         .font(.title)
 
 				}
