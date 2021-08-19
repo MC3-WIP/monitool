04ab6340c33cd3ff778e65b80205f9b693030f7e
Naufaldi Athallah Rifqi <39259177+naufaldirfq@users.noreply.github.com>
2021-08-19 16:09:31 +0700

    Feature/add task iphone (#79)
    * add task for iphone
    
    * reformat code
    
    * add task view not used anymore
    
    * delete unused code

--- monitool/Views/iPhone/Components/Tab Items/TaskListTabItem.swift
+++ monitool/Views/iPhone/Components/Tab Items/TaskListTabItem.swift
@@ -25,9 +25,9 @@ struct TaskListTabItem: View {
 				AddTaskButton()
 			}
 		}
-		.sheet(isPresented: $showSheetView) {
-			AddTaskView(showSheetView: $showSheetView)
-		}
+        .popover(isPresented: $showSheetView) {
+            AddDataPopOver(sheetType: "Task", showingPopOver: $showSheetView)
+        }
 		.tabItem {
 			Image(systemName: "text.badge.plus")
 			Text("Task List")
