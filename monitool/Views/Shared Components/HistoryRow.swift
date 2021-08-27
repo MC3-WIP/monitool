e8ca6aa08d5be032c19cab4c0f8bf07c6e682a48
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-08-27 10:59:48 +0700

    Update history view

--- monitool/Views/Shared Components/HistoryRow.swift
+++ monitool/Views/Shared Components/HistoryRow.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 import UIKit
 
+class HistoryRowModel: ObservableObject {
+	@Published var taskTitle: String
+	@Published var picName = "-"
+
+	init(task: Task) {
+		taskTitle = task.name
+		task.pic?.getDocument { [self] doc, err in
+			if let err = err {
+				print("Error occured while getting PIC data.", err.localizedDescription)
+				return
+			}
+
+			guard let doc = doc else { return }
+
+			do {
+				if let pic = try doc.data(as: Employee.self) {
+					picName = pic.name
+				}
+			} catch {
+				print("Unable to parse employee name")
+			}
+		}
+	}
+}
+
 struct HistoryRow: View {
-    @StateObject var taskDetailViewModel: TaskDetailViewModel
+    @StateObject var viewModel: HistoryRowModel
 
     init(task: Task) {
-        _taskDetailViewModel = StateObject(wrappedValue: TaskDetailViewModel(task: task))
+        _viewModel = StateObject(wrappedValue: HistoryRowModel(task: task))
     }
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text(taskDetailViewModel.task.name)
-            Text("PIC: \(taskDetailViewModel.pic?.name ?? "-")")
+			Text(viewModel.taskTitle)
+			Text("PIC: \(viewModel.picName)")
                 .font(.caption)
                 .foregroundColor(Color("DarkGray"))
         }.listRowBackground(Color("LightTosca"))
--- monitool/Views/iPad/Components/HistoryView.swift
+++ monitool/Views/iPad/Components/HistoryView.swift
@@ -8,67 +8,78 @@
 import SwiftUI
 
 struct HistoryView: View {
-    @StateObject private var taskViewModel = TaskViewModel()
-    private let dateHelper = DateHelper()
+	@StateObject private var taskViewModel = TaskViewModel()
+	private let dateHelper = DateHelper()
 
-    init() {
-        UITableView.appearance().backgroundColor = .clear
-    }
+	init() {
+		UITableView.appearance().backgroundColor = .clear
+	}
 
-    var body: some View {
-        List {
-            ForEach(0 ..< 6) { index in
-                if taskViewModel.historiesPerDay[index].count == 0 {
-                    EmptyView()
-                } else {
-                    HistoriesSection(histories: taskViewModel.historiesPerDay[index])
-                        .padding(.vertical, 5)
-                }
-            }
-        }
-        .navigationBarTitle("History", displayMode: .inline)
-        .listStyle(InsetGroupedListStyle())
-    }
+	var body: some View {
+		List {
+			ForEach(0 ..< 6) { index in
+				if taskViewModel.historiesPerDay[index].count == 0 {
+					EmptyView()
+				} else {
+					HistoriesSection(histories: taskViewModel.historiesPerDay[index])
+						.padding(.vertical, 5)
+				}
+			}
+		}
+		.navigationBarTitle("History", displayMode: .inline)
+		.listStyle(InsetGroupedListStyle())
+	}
 }
 
 struct HistoriesSection: View {
-    private let device = UIDevice.current.userInterfaceIdiom
+	private let device = UIDevice.current.userInterfaceIdiom
 	private let dateHelper = DateHelper()
 
 	var histories: [Task]
-    var day: String
+	var day: String
 
-    init(histories: [Task]) {
-        self.histories = histories
-        if dateHelper.getNumDays(first: histories[0].createdAt, second: Date()) == 0 {
-            day = "Today"
-        } else {
-            day = dateHelper.getStringFromDate(date: histories[0].createdAt)
-        }
-    }
+	init(histories: [Task]) {
+		self.histories = histories
 
-    var body: some View {
-		Section(header: Text(day).font(.title.bold()).foregroundColor(AppColor.primaryBackground)) {
-            ForEach(histories, id: \.id) { history in
-                if device == .pad {
-                    NavigationLink(destination: HistoryTaskDetailView(task: history)) {
-                    }
+		let dayGap = dateHelper.getNumDays(first: histories[0].createdAt, second: Date())
+
+		if dayGap == 0 {
+			day = "Today"
+		} else if dayGap == 1 {
+			day = "Yesterday"
+		} else {
+			day = dateHelper.getStringFromDate(date: histories[0].createdAt)
+		}
+	}
+
+	var body: some View {
+		Section(
+			header: Text(day)
+				.font(.title.bold())
+				.foregroundColor(AppColor.primaryBackground)
+		) {
+			ForEach(histories, id: \.id) { history in
+				if device == .pad {
+					NavigationLink(destination: HistoryTaskDetailView(task: history)) {
+						HistoryRow(task: history)
+					}
 					.listRowBackground(AppColor.lightAccent)
-                } else if device == .phone {
-                    NavigationLink(destination: HistoryDetail(history: history)) {
-						
-                    }
+				} else if device == .phone {
+					NavigationLink(destination: HistoryDetail(history: history)) {
+						HistoryRow(task: history)
+					}
 					.listRowBackground(AppColor.lightAccent)
-                }
-            }
-        }
-        .listStyle(PlainListStyle())
-        .navigationTitle("History")
-    }
+				}
+			}
+		}
+		.textCase(.none)
+		.listStyle(PlainListStyle())
+		.navigationTitle("History")
+	}
 }
 
 struct HistoryView_Previews: PreviewProvider {
-    static var previews: some View {
-        HistoryView()
-    }
+	static var previews: some View {
+		HistoryView()
+	}
 }
